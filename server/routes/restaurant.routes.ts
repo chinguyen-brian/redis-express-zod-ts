a9127865cc0932e4cc28110780e908844c1abe14
Chi Nguyen <nguyennc1303@gmail.com>
2025-10-30 16:35:30 +0700

    Step 9: String + cache-aside (Weather API)

--- server/routes/restaurant.routes.ts
+++ server/routes/restaurant.routes.ts
@@ -14,6 +14,7 @@ import {
   restaurantsByRatingKey,
   reviewDetailsKeyById,
   reviewKeyById,
+  weatherKeyById,
 } from "../utils/redis/keys.js";
 import { errorResponse, successResponse } from "../utils/responses.js";
 import { checkRestaurantExists } from "../middlewares/checkRestaurantId.js";
@@ -86,6 +87,42 @@ router.get(
         { ...restaurant, cuisines },
         "Got restaurant by id"
       );
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.get(
+  "/:restaurantId/weather",
+  checkRestaurantExists,
+  async (req: Request<{ restaurantId: string }>, res, next) => {
+    const { restaurantId } = req.params;
+    try {
+      const client = await initializeRedisClient();
+      const weatherKey = weatherKeyById(restaurantId);
+      const cachedWeather = await client.get(weatherKey);
+      if (cachedWeather) {
+        console.log("Cached hit");
+        return successResponse(res, JSON.parse(cachedWeather));
+      }
+      const restaurantKey = restaurantKeyById(restaurantId);
+      const coords = await client.hGet(restaurantKey, "location");
+      if (!coords) {
+        return errorResponse(res, 404, "Coordinates not found");
+      }
+      const [lng, lat] = coords.split(",");
+      const apiResponse = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?units=imperial&lat=${lat}&lon=${lng}&appid=${process.env.WEATHER_API_KEY}`
+      );
+      if (apiResponse.status === 200) {
+        const json = await apiResponse.json();
+        await client.set(weatherKey, JSON.stringify(json), {
+          expiration: { type: "EX", value: 60 * 60 },
+        });
+        return successResponse(res, json);
+      }
+      return errorResponse(res, 500, "Couldn't fetch weather info");
     } catch (error) {
       next(error);
     }
